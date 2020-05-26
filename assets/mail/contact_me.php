c7cbe0d07e2eb062debb4c19cb6793ecf3768c8d
Ben Moore <ben.karl.moore@gmail.com>
2020-05-25 22:50:08 -0700

    Update contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -9,6 +9,7 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
+echo "here..."
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
