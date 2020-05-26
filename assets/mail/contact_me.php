a898db3daa869783f8166b840f435e2987f14ebc
Ben Moore <ben.karl.moore@gmail.com>
2020-05-25 22:49:04 -0700

    Update contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -15,7 +15,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-echo $name $email_address $phone $message;
+echo $name . "\n" . $email_address . "\n" . $phone . "\n" . $message;
 // Create the email and send the message
 $to = 'analyticalrobotics@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
